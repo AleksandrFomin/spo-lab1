76d431162bbcdd8245cb578f4188592126575290
Aleksandr Fomin <fsasha_1997@mail.ru>
2019-10-03 22:33:46 +0300

    done

--- lab1.c
+++ lab1.c
@@ -10,25 +10,18 @@
 #include <getopt.h>
 #include <inttypes.h>
 
-// #define STDIN_FILENO 0
-// #define STDOUT_FILENO 1
-// #define STDERR_FILENO 2
 #define BUFFER_SIZE 1024
 
 static uintmax_t total_lines;
-static uintmax_t total_words;
-static uintmax_t total_chars;
 static uintmax_t total_bytes;
 
-static int print_lines, print_words, print_chars, print_bytes;
+static int print_lines, print_bytes;
 static int have_read_stdin;
 
 static struct option const longopts[] =
 {
 	{"bytes", no_argument, NULL, 'c'},
-	{"chars", no_argument, NULL, 'm'},
 	{"lines", no_argument, NULL, 'l'},
-	{"words", no_argument, NULL, 'w'},
 	{"help", no_argument, NULL, 'h'},
 	{NULL, 0, NULL, 0}
 };
@@ -40,24 +33,22 @@ void print_error(int error)
 	exit(error);
 }
 
-void write_counts (uintmax_t lines, uintmax_t bytes, const char *file)
+void write_counts(uintmax_t lines, uintmax_t bytes, const char *file)
 {
 	if (print_bytes) {
-		char tmp[64] = {0x0};
-		sprintf(tmp, "%lu", bytes);
-		write(STDOUT_FILENO, tmp, sizeof(tmp));
-		write(STDOUT_FILENO, " ", 1);
+		char str[64] = {0x0};
+		int size = sprintf(str, "%lu ", bytes);
+		write(STDOUT_FILENO, str, size);
 	}
 
 	if (print_lines) {
-		char tmp[64] = {0x0};
-		sprintf(tmp, "%lu", bytes);
-		write(STDOUT_FILENO, tmp, sizeof(tmp));
-		write(STDOUT_FILENO, " ", 1);
+		char str[64] = {0x0};
+		int size = sprintf(str, "%lu ", lines);
+		write(STDOUT_FILENO, str, size);
 	}
 
 	if (file) {
-		write(STDOUT_FILENO, file, sizeof(file));
+		write(STDOUT_FILENO, file, strlen(file));
 	}
 	write(STDOUT_FILENO, "\n", 1);
 }
@@ -66,9 +57,9 @@ int wc(int fd, char const *file_name)
 {
 	char buf[BUFFER_SIZE + 1];
 	size_t bytes_read;
-	uintmax_t lines, words, bytes;
-	// printf("file: %s\n", file_name);
-	lines = words = bytes = 0;
+	uintmax_t lines, bytes;
+	lines = bytes = 0;
+
 	while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		char *p = buf;
@@ -79,12 +70,12 @@ int wc(int fd, char const *file_name)
 		}
 		bytes += bytes_read;
 	}
-	write_counts (lines, bytes, file_name);
-  	total_lines += lines;
-  	total_bytes += bytes;
 
-  	return 0;
-	// printf("file: %s bytes: %lu lines: %lu words: %lu \n", file_name, bytes, lines, words);
+	write_counts (lines, bytes, file_name);
+	total_lines += lines;
+	total_bytes += bytes;
+
+	return 0;
 }
 
 int wc_file(char const *file)
@@ -128,35 +119,22 @@ int main(int argc, char* argv[])
 	int nfiles;
 	char **files;
 	int optc;
-	print_lines = print_words = print_chars = print_bytes = 0;
-	total_lines = total_words = total_chars = total_bytes = 0;
+	print_lines = print_bytes = 0;
+	total_lines = total_bytes = 0;
 
-	while ((optc = getopt_long (argc, argv, "clmwh", longopts, NULL)) != -1)
+	while ((optc = getopt_long (argc, argv, "clh", longopts, NULL)) != -1)
 	{
 		switch (optc)
 		{
 		case 'c':
 			print_bytes = 1;
-			// write(STDERR_FILENO, "c\n", 2);
-			break;
-
-		case 'm':
-			print_chars = 1;
-			// write(STDERR_FILENO, "m\n", 2);
 			break;
 
 		case 'l':
 			print_lines = 1;
-			// write(STDERR_FILENO, "l\n", 2);
-			break;
-
-		case 'w':
-			print_words = 1;
-			// write(STDERR_FILENO, "w\n", 2);
 			break;
 
 		case 'h':
-			// write(STDERR_FILENO, "h\n", 2);
 			usage();
 			break;
 
@@ -166,15 +144,21 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	static char *stdin_only[] = { NULL };
-	// files = (optind < argc ? argv + optind : stdin_only);
-	// nfiles = (optind < argc ? argc - optind : 1);
+	if (!print_bytes && !print_lines)
+		print_bytes = print_lines = 1;
 
-	//Parse extra arguments
+	static char *stdin_only[] = { NULL };
+
+	int count = 0;
 	for (; optind < argc; optind++)
 	{
-		// printf("extra arguments: %s\n", argv[optind]);
+		count++;
 		wc_file(argv[optind]);
 	}
 
+	if (count > 1) {
+		write_counts(total_lines, total_bytes, "итого");
+	}
+
+	exit(0);
 }
